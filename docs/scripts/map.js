008b45ce92f8416ee00611ac73898237f1cd66c5
lkieliger <leandro.kieliger@epfl.ch>
2017-11-07 14:10:55 +0100

    Layout map, disable scrolling with wheel and rectify display order

--- docs/scripts/map.js
+++ docs/scripts/map.js
@@ -1,9 +1,20 @@
 console.log("Calling map.js")
 
-var testMap = L.map('mapid').setView([38.338319, 18.466935], 2);
+d3.select("#mapid")
+    .style("height", 500)
+    .style("width", width)
+    .style("left", margin.left)
+    .style("top", margin.top);
+
+let testMap = L.map('mapid', {
+	center: [38.338319, 18.466935],
+	zoom: 2,
+	scrollWheelZoom:false
+});
 
 L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
 	subdomains: 'abcd',
 	maxZoom: 10
 }).addTo(testMap);
+
