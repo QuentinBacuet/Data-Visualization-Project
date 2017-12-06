7559cd7c51363a81264571a066ba735473900a58
QuentinBacuet <quentin.bacuet@epfl.ch>
2017-12-06 16:11:10 +0100

    Correct little bug

--- docs/scripts/country_graph.js
+++ docs/scripts/country_graph.js
@@ -62,13 +62,12 @@ country_graph.update_graph = function (country_code) {
                 i_entry++;
             } else {
                 data_entry.push(0);
-                data_entry.push(0);
             }
             if (data_immigration_exit_slice[i_exit] !== undefined && data_immigration_exit_slice[i_exit].year === (timevals.min_year + i).toString()) {
                 data_exit.push(country_graph.domainOnlyScale_up(data_immigration_exit_slice[i_exit].value));
                 i_exit++;
             } else {
-                data_exit.push(0)
+                data_exit.push(0);
             }
 
             data_diff.push(data_entry[i] - data_exit[i])
